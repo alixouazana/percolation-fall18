e5ec8282da4dad5ac78307f4e693d502c4c8de47
Alix Ouazana <alix.ouazana@duke.edu>
2018-11-28 17:46:01 -0500

    message

---
+++ src/PercolationBFS.java
@@ -0,0 +1,33 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class PercolationBFS extends PercolationDFSFast{
+
+	public PercolationBFS(int n) {
+		super(n);
+	}
+	
+	@Override
+	protected void dfs(int row, int col) {
+		Queue<Integer> qp = new LinkedList<>();
+		int cell = myGrid[row][col];
+		cell = FULL;
+		qp.add(cell);
+		
+		
+		// out of bounds?
+		if (! inBounds(row,col)) return;
+		
+		// full or NOT open, don't process
+		if (isFull(row, col) || !isOpen(row, col))
+			return;
+		
+		myGrid[row][col] = FULL;
+		dfs(row - 1, col);
+		dfs(row, col - 1);
+		dfs(row, col + 1);
+		dfs(row + 1, col);
+	}
+
+
+}
---
+++ src/PercolationDFSFast.java
@@ -0,0 +1,20 @@
+
+public class PercolationDFSFast extends PercolationDFS{
+
+	public PercolationDFSFast(int n) {
+		super(n);
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	protected void updateOnOpen(int row, int col) {
+		if(row == 0) myGrid[row][col] = FULL;
+		if(myGrid[row-1][col] == FULL || myGrid[row][col+1] == FULL|| myGrid[row][col-1] == FULL) {
+			myGrid[row][col] = FULL;
+		}
+		if(myGrid[row][col]== FULL) dfs(row,col);
+	}
+
+}
+
+
