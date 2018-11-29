cfea3682061075c4a781de81a0f42fcd4cc86cab
Alix Ouazana <alix.ouazana@duke.edu>
2018-11-29 12:20:23 -0500

    m

--- src/PercolationDFSFast.java
+++ src/PercolationDFSFast.java
@@ -8,9 +8,26 @@ public class PercolationDFSFast extends PercolationDFS{
 	
 	@Override
 	protected void updateOnOpen(int row, int col) {
-			if(row == 0 || isFull(row+1, col) || isFull(row,col+1) || isFull(row-1,col) || isFull(row,col-1)) {
+			
+		
+			if(row == 0) {
 				dfs(row,col);
 			}
+			if(row !=0 && isFull(row-1,col)) {
+				dfs(row,col);
+			}
+			if(row != myGrid.length && isFull(row+1, col)){
+				dfs(row,col);
+			}
+			
+			if(col != 0 && isFull(row,col-1)) {
+				dfs(row,col);
+			}
+			if(col != myGrid[0].length && isFull(row,col+1)) {
+				dfs(row,col);
+			}
+			
+			
 		
 	}
 
