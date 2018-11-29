98252fa8ea819dc862c1a893d4f1fea6ad4b8b94
Alix Ouazana <alix.ouazana@duke.edu>
2018-11-29 18:19:49 -0500

    mess

--- src/PercolationBFS.java
+++ src/PercolationBFS.java
@@ -9,24 +9,36 @@ public class PercolationBFS extends PercolationDFSFast{
 	
 	@Override
 	protected void dfs(int row, int col) {
-		Queue<Integer> qp = new LinkedList<>();
-		int cell = myGrid[row][col];
-		cell = FULL;
-		qp.add(cell);
-		
-		
 		// out of bounds?
 		if (! inBounds(row,col)) return;
 		
-		// full or NOT open, don't process
-		if (isFull(row, col) || !isOpen(row, col))
-			return;
+		int size = myGrid.length;
+		Queue<Integer> q = new LinkedList<>();
 		
 		myGrid[row][col] = FULL;
-		dfs(row - 1, col);
-		dfs(row, col - 1);
-		dfs(row, col + 1);
-		dfs(row + 1, col);
+		q.add(myGrid[row][col]);
+		
+		while(q.size()!=0) {
+			int newcell = q.remove();
+			row = newcell/size;
+			col = newcell%size;
+
+			if(inBounds(row-1, col) && isOpen(row-1, col) && !isFull(row, col)) {
+				q.add(myGrid[row-1][col]);
+			}
+			if(inBounds(row, col-1) && isOpen(row, col-1) && !isFull(row, col-1)) {
+				q.add(myGrid[row][col-1]);
+			}
+			if(inBounds(row, col+1) && isOpen(row, col+1) && !isFull(row, col+1)) {
+				q.add(myGrid[row][col+1]);
+			}	
+
+			if(inBounds(row+1, col) && isOpen(row+1, col) && !isFull(row+1, col)) {
+				q.add(myGrid[row+1][col]);
+			}
+		}
+		
+	
 	}
 
 
