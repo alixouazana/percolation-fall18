9b06fdca4e52295baa6c3f73637f97ca7f85d2b3
Alix Ouazana <alix.ouazana@duke.edu>
2018-11-30 20:33:09 -0500

    m

--- src/PercolationBFS.java
+++ src/PercolationBFS.java
@@ -31,16 +31,16 @@ public class PercolationBFS extends PercolationDFSFast{
 				myGrid[row-1][col] = FULL;
 				q.add((row-1)*myGrid.length + col);
 			}
-			else if(inBounds(row, col-1) && isOpen(row, col-1) && !isFull(row, col-1)) {
+			if(inBounds(row, col-1) && isOpen(row, col-1) && !isFull(row, col-1)) {
 				myGrid[row][col-1] = FULL;
 				q.add(row*myGrid.length + (col-1));
 			}
-			else if(inBounds(row, col+1) && isOpen(row, col+1) && !isFull(row, col+1)) {
+			if(inBounds(row, col+1) && isOpen(row, col+1) && !isFull(row, col+1)) {
 				myGrid[row][col+1] = FULL;
 				q.add(row*myGrid.length + (col+1));
 			}	
 
-			else if(inBounds(row+1, col) && isOpen(row+1, col) && !isFull(row+1, col)) {
+			if(inBounds(row+1, col) && isOpen(row+1, col) && !isFull(row+1, col)) {
 				myGrid[row+1][col] = FULL;
 				q.add((row+1)*myGrid.length + col);
 			}
